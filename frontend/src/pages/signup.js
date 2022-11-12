1f9f112c6ce9bf7fc5d7d2d31bc8ec617f214592
Raphael FB <jvstraphael@gmail.com>
2022-11-12 22:29:04 +0200

    preparing for database

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,35 +1,40 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { HeaderContainer, FooterContainer } from "../containers";
 import { Signup, Form } from "../components";
 import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import { Input } from "../components/form/styles/form";
 
 const Signupp = () => {
-  const [input, setInput] = useState({
-    title: '',
-    content: ''
-  })
-  function handleChange(event) {
-    const {name, value} = event.target;
+    const validationSchema = Yup.object().shape({
+        fullname: Yup.string().required('Fullname is required'),
+        email: Yup.string()
+          .required('Email is required')
+          .email('Email is invalid'),
+        password: Yup.string()
+          .required('Password is required')
+          .min(6, 'Password must be at least 6 characters')
+          .max(40, 'Password must not exceed 40 characters'),
+        confirmPassword: Yup.string()
+          .required('Confirm Password is required')
+          .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
+      });
 
-    setInput(prevInput => {
-      return {
-        ...prevInput,
-        [name]: value
-      }
-    })
-  }
-
-  function handleClick(event) {
-    event.preventDefault();
-    const newNote = {
-      title: input.title,
-      email: input.email,
-      password: input.password
-    }
+      const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+      } = useForm({
+        resolver: yupResolver(validationSchema)
+      });
     
-    axios.post('http://localhost:8000/create', newNote)
+      const onSubmit = data => {
+        console.log(JSON.stringify(data, null, 2));
   }
-
   return (
     <Fragment>
       <HeaderContainer bg="false" />
@@ -40,25 +45,29 @@ const Signupp = () => {
               <Signup.Title>Signup</Signup.Title>
             </Signup.Header>
             <Signup.InnerContent>
-              <Form>
+              <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.FormGroup>
-                  <Form.Label onChange={handleChange}>Name</Form.Label>
-                  <Form.Input onChange={handleChange} name="title" value={input.title} type="text" />
+                  <Form.Label>Full Name</Form.Label>
+                  <Form.Input name="title" value={Form.Input.title} type="text" {...register('fullname')} className={`form-control ${errors.fullname ? 'is-invalid' : ''}`}/>
+                  <div className="invalid-feedback">{errors.fullname?.message}</div>
                 </Form.FormGroup>
                 <Form.FormGroup>
-                  <Form.Label onChange={handleChange}>Email</Form.Label>
-                  <Form.Input onChange={handleChange} name="email" value={input.email} type="text" />
+                  <Form.Label>Email</Form.Label>
+                  <Form.Input name="email" value={Form.Input.email} type="text" {...register('email')} className={`form-control ${errors.email ? 'is-invalid' : ''}`}/>
+                  <div className="invalid-feedback">{errors.email?.message}</div>
                 </Form.FormGroup>
                 <Form.FormGroup>
-                  <Form.Label onChange={handleChange}>Password</Form.Label>
-                  <Form.Input onChange={handleChange} name="password" value={input.password} type="text" />
+                  <Form.Label>Password</Form.Label>
+                  <Form.Input name="password" value={Form.Input.password} type="text" {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`}/>
+                  <div className="invalid-feedback">{errors.password?.message}</div>
                 </Form.FormGroup>
                 <Form.FormGroup>
-                  <Form.Label onChange={handleChange}>Confirm Password</Form.Label>
-                  <Form.Input onChange={handleChange} name="confimation" value={input.confirmation} type="text" />
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Input name="confimation" value={Form.Input.confirmation} type="text"  {...register('confirmPassword')} className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}/>
+                  <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
                 </Form.FormGroup>
                 <Form.FormGroup>
-                  <Form.SubmitInput onClick={handleClick} type="submit" value="Signup" />
+                  <Form.SubmitInput type="submit" value="Signup" />
                 </Form.FormGroup>
               </Form>
             </Signup.InnerContent>
