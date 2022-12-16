6c86cbe11ae52d7669fc36f536a1ed98e319ae22
Raphael FB <jvstraphael@gmail.com>
2022-12-16 18:30:57 +0200

    adding react-toastify on the contact page

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,27 +1,30 @@
 import "./ContactFormStyles.css";
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function ContactForm() {
   const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-
+  const handleSubmit = event => {
+    event.preventDefault();
+    // perform form validation and submission logic here
     emailjs.sendForm('service_mk5j6fc', 'template_u43pxkb', form.current, 'alhwLOSp2yLNOem4D')
       .then((result) => {
           console.log(result.text);
-          console.log("booking sent")
+          console.log("form sent");
+          toast("Form submitted successfully!");
+          event.target.reset();
         }, (error) => {
             console.log(error.text);
         });
-        e.target.reset()
-  };
+  }
 
   return (
     <div className="form-container">
       <h1>Send us a message we'll get back to you!</h1>
-      <form ref={form} onSubmit={sendEmail}>
+      <form ref={form} onSubmit={handleSubmit}>
         <label>Full Name</label>
         <input name="name" placeholder="Name"/>
         <label>Email</label>
@@ -31,7 +34,7 @@ function ContactForm() {
         <label>Message</label>
         <textarea name="message" placeholder="Message" rows={4}/>
         <button>Send Message</button>
-        <p>Please Note after submitting the form will reset and you can await response</p>
+        <ToastContainer />
       </form>
     </div>
   );
--- frontend/src/components/PopUpChat.js
+++ frontend/src/components/PopUpChat.js
@@ -1,70 +1,68 @@
-import React from 'react';
-import './PopUp.css';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Avataaars } from 'avataaars';
 
+const PopupChat = () => {
+  const [showChat, setShowChat] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
 
-function PopUpChat() {
-    const popup = document.querySelector('.chat-popup');
-    const chatBtn = document.querySelector('.chat-btn');
-    const submitBtn = document.querySelector('.submit')
-    const chatArea = document.querySelector('.chat-area')
-    const inputElm = document.querySelector('input')
-    const emojiBtn = document.querySelector('#emoji-btn');
-    const picker = new EmojiButton();
+  // Generate random avatars for the user and the admin
+  const userAvatar = <Avataaars />;
+  const adminAvatar = <Avataaars />;
 
+  const handleChatToggle = () => {
+    setShowChat(!showChat);
+  };
 
-// Emoji selection 
-window.addEventListener('DOMContentLoaded', () => {
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
 
-    picker.on('emoji', emoji => {
-        document.querySelector('input').value += emoji;
-    })
+  const handleMessageSubmit = (event) => {
+    event.preventDefault();
+    setMessages([...messages, { sender: 'user', message: message, avatar: userAvatar }]);
+    setMessage('');
+  };
 
-    emojiBtn.addEventListener('click', () => {
-        picker.togglePicker(emojiBtn);
-    });
-});
-
-// chat button toggler
-
-chatBtn.addEventListener('click', ()=>{
-    popup.classList.toggle('show');
-})
-
-// send msg 
-submitBtn.addEventListener('click', ()=>{
-    let userInput = inputElm.value;
-
-    let temp = `<div className="out-msg">
-    <span className="my-msg">${userInput}</span>
-    <img src="img/raphael.jpg" className="avatar"
-    </div>`;
-
-    chatArea.insertAdjacentHTML("beforeend", temp);
-    inputElm.value = "";
-})
   return (
-    <section>
-        <button className="chat-btn">
-            <i className="material-icons"> comment </i>
-        </button>
-
-        <div className="chat-popup">
-            <div className="badge">1</div>
-            <div className="chat-area">
-             <div className="income-msg">
-                <img src="img/sakhile.jpeg" className="avatar" alt=""/>
-                <span className="msg"> Hi, How can I help you?</span>
-             </div>
-            </div>
-
-            <div className="input-area">
-                <input type="text"/>
-                <button id="emoji-btn"> &#127773;</button>
-                <button className="submit"> <i className="material-icons"> send</i></button>
-            </div>
+    <div>
+      { showChat ? (
+        <div className="popup-chat-container">
+          <div className="chat-header">
+            <FontAwesomeIcon icon={faEnvelope} size="lg" />
+            <h3>Chat</h3>
+            <button onClick={handleChatToggle}>Close</button>
+          </div>
+          <div className="chat-messages">
+            {messages.map((msg, index) => (
+              <div key={index} className="chat-message">
+                {msg.avatar}
+                <div className={`message ${msg.sender}`}>{msg.message}</div>
+              </div>
+            ))}
+          </div>
+          <div className="chat-input">
+            <form onSubmit={handleMessageSubmit}>
+              <input
+                type="text"
+                placeholder="Enter message..."
+                value={message}
+                onChange={handleMessageChange}
+              />
+              <button type="submit">Send</button>
+            </form>
+          </div>
         </div>
-    </section>
-  )
-}
+      ) : (
+        <button onClick={handleChatToggle}>
+          <FontAwesomeIcon icon={faEnvelope} size="lg" />
+          Open Chat
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default PopUpChat
+export default PopupChat;
